b16f4792be749474846dfce44be5ab1a58ed7224
Tony Kerr <46275713+PoeticDeath@users.noreply.github.com>
2026-01-16 12:04:44 -0500

    V1.2.0

--- FSTest.py
+++ FSTest.py
@@ -10,6 +10,7 @@ os.mkdir("FSTest")
 
 totcre = 0
 totread = 0
+totalloc = 0
 totwrite = 0
 i = 0
 
@@ -18,7 +19,13 @@ try:
         tmpcre = time()
         c = open("FSTest/" + hex(i)[2:], "wb+")
         totcre += time() - tmpcre
-        data = randbytes(randint(1, 2097152))#67108864))
+        size = randint(1, 2097152)
+        data = randbytes(size)
+        tmpalloc = time()
+        c.write(bytes(size))
+        c.flush()
+        totalloc += time() - tmpalloc
+        c.seek(0)
         tmpwrite = time()
         c.write(data)
         c.flush()
@@ -28,10 +35,23 @@ try:
         d = c.read()
         totread += time() - tmpread
         if d != data:
-            print(f"Error on file: {i}   ")
+            print(f"Error on file: {hex(i)[2:]}   ")
         if i % 100 == 0:
-            print(f"Create Time: {totcre}, Read Time: {totread}, Write Time: {totwrite}, Files: {i + 1}   ", end="\r")
+            print(f"Create Time: {totcre:.2f} {totcre/(i+1):.5f}, Read Time: {totread:.2f} {totread/(i+1):.5f}, Allocation Time: {totalloc-totwrite:.2f} {(totalloc-totwrite)/(i+1):.5f}, Write Time: {totwrite:.2f} {totwrite/(i+1):.5f}, Files: {i + 1}   ", end="\r")
         i += 1
 except Exception as e:
     print(e)
-    print(f"Create Time: {totcre}, Read Time: {totread}, Write Time: {totwrite}, Files: {i + 1}   ")
+    print(f"Create Time: {totcre:.2f} {totcre/(i+1):.5f}, Read Time: {totread:.2f} {totread/(i+1):.5f}, Allocation Time: {totalloc-totwrite:.2f} {(totalloc-totwrite)/(i+1):.5f}, Write Time: {totwrite:.2f} {totwrite/(i+1):.5f}, Files: {i + 1}   ")
+
+seed(0)
+for i in os.listdir("FSTest/"):
+    size = randint(1, 2097152)
+    data = randbytes(size)
+    if os.path.getsize("FSTest/" + i) == size:
+        c = open("FSTest/" + i, "rb").read()
+        if c != data:
+            print(f"Error on file: {i}   ")
+    if int(i, 16) % 100 == 0:
+        print(f"Double Checking: {int(i, 16)}   ", end="\r")
+print(f"Double Checking: {int(i, 16) + 1}   ")
+shutil.rmtree("FSTest")
