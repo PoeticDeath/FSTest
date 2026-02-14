c817222a2b7b0e7fe9a764ebe586129070639eae
Tony Kerr <46275713+PoeticDeath@users.noreply.github.com>
2026-02-14 12:51:20 -0500

    V1.7.0

--- FSTest.py
+++ FSTest.py
@@ -3,10 +3,15 @@ import shutil
 from time import time
 from random import seed, randint, randbytes
 
+path = input("Full Test Path: ")
+if not path.startswith("/"):
+    print("Must be full path.")
+    exit()
+
 seed(0)
-if "FSTest" in os.listdir():
-    shutil.rmtree("FSTest")
-os.mkdir("FSTest")
+if "FSTest" in os.listdir(path):
+    shutil.rmtree(path + "/FSTest")
+os.mkdir(path + "/FSTest")
 
 totcre = 0
 totread = 0
@@ -17,7 +22,7 @@ i = 0
 try:
     while True:
         tmpcre = time()
-        c = open("FSTest/" + hex(i)[2:], "wb+")
+        c = open(path + "/FSTest/" + hex(i)[2:], "wb+")
         totcre += time() - tmpcre
         size = randint(1, 2097152)
         data = randbytes(size)
@@ -43,16 +48,22 @@ try:
             print(f"Create Time: {totcre:.2f} {totcre/(i+1):.5f}, Read Time: {totread:.2f} {totread/(i+1):.5f}, Allocation Time: {totalloc:.2f} {totalloc/(i+1):.5f}, Write Time: {totwrite:.2f} {totwrite/(i+1):.5f}, Files: {i + 1}   ", end="\r")
         i += 1
 except (Exception,KeyboardInterrupt) as e:
+    try:
+        c.close()
+    except NameError:
+        pass
     print(e)
     print(f"Create Time: {totcre:.2f} {totcre/(i+1):.5f}, Read Time: {totread:.2f} {totread/(i+1):.5f}, Allocation Time: {totalloc:.2f} {totalloc/(i+1):.5f}, Write Time: {totwrite:.2f} {totwrite/(i+1):.5f}, Files: {i + 1}   ")
 
+input("Unmount and Remount: ")
+
 seed(0)
-for i in sorted([int(i, 16) for i in os.listdir("FSTest/")]):
+for i in sorted([int(i, 16) for i in os.listdir(path + "/FSTest/")]):
     i = hex(i)[2:]
     size = randint(1, 2097152)
     data = randbytes(size)
-    if os.path.getsize("FSTest/" + i) == size:
-        c = open("FSTest/" + i, "rb")
+    if os.path.getsize(path + "/FSTest/" + i) == size:
+        c = open(path + "/FSTest/" + i, "rb")
         d = c.read()
         c.close()
         if d != data:
@@ -60,4 +71,4 @@ for i in sorted([int(i, 16) for i in os.listdir("FSTest/")]):
     if int(i, 16) % 100 == 0:
         print(f"Double Checking: {int(i, 16) + 1}   ", end="\r")
 print(f"Double Checking: {int(i, 16) + 1}   ")
-shutil.rmtree("FSTest")
+shutil.rmtree(path + "/FSTest")
